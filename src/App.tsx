d996094655da221e4ea25d8e47f36fa0b1a5343c
Galih A <galih.asmarandaru@gmail.com>
2024-12-10 09:38:53 +0700

    improve: add count advice and use props using typescript

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,18 @@
 import { useEffect, useState } from 'react'
 
+interface MessageProps {
+  count: number;
+}
+
 function App() {
   const [advice, setAdvice] = useState()
+  const [count, setCount] = useState(0)
 
   async function getAdvice() {
     const res = await fetch(import.meta.env.VITE_API_URL_ADVICE)
     const data = await res.json()
     setAdvice(data.slip.advice)
+    setCount((c) => c + 1)
   }
 
   useEffect(() => { getAdvice() }, [])
@@ -15,7 +21,16 @@ function App() {
     <>
       <h1>{advice}</h1>
       <button onClick={getAdvice}>Get Advice</button>
+      <Message count={count} />
     </>
+  )
+}
+
+function Message(props: MessageProps) {
+  return (
+    <p>
+      Anda telah mendapatkan advice sebanyak <strong>{props.count}</strong> kali
+    </p>
   )
 }
 
